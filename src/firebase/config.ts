fb97babf481fb5220e5611d88fc7731c7c17a5f5
Steave Tetos <Tetos7007@gmail.com>
2025-11-14 21:55:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8126703845-ecb31",
+  "appId": "1:147985643810:web:7c82aaaaa3ea452f2f0d7b",
+  "apiKey": "AIzaSyDPsnPhHI_LfdKbqo8bwRVXp5fS0RuEISw",
+  "authDomain": "studio-8126703845-ecb31.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "147985643810"
+};
